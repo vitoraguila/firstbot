3d2c2a0601a44baec1bf62e5aa5ae5f483a30eb4
Vitor Hugo Aguila Oliveira <vitor.aguila1@gmail.com>
2018-03-01 23:39:22 -0300

    configure db and seed

--- config/database.rb
+++ config/database.rb
@@ -20,13 +20,16 @@ set :database, {
 }
 end
 
+
 configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+
   set :database, {
-    adapter: 'postgresql',
-    encoding: 'utf8',
-    database: 'firstbot_production',
-    pool: 5,
-    username: 'postgres',
-    host: 'postgres'
+    adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
   }
 end
